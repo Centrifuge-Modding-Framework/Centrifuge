41b39b7248a50ffa63b6da21123313e9a9498460
Ciastex <Ciastex@users.noreply.github.com>
2019-08-12 13:48:01 +0200

    Added Game API, limited for now.

---
+++ Reactor.API.GTTOD/GameAPI.cs
@@ -0,0 +1,28 @@
+﻿using CommandTerminal;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Reactor.API.GTTOD
+{
+    public class GameAPI : MonoBehaviour
+    {
+        public static event EventHandler TerminalInitialized;
+
+        public void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void Start()
+        {
+            StartCoroutine(WaitForTerminal());
+        }
+
+        IEnumerator WaitForTerminal()
+        {
+            yield return new WaitUntil(() => FindObjectOfType<Terminal>());
+            TerminalInitialized?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
--- Reactor/Global.cs
+++ Reactor/Global.cs
@@ -1,11 +1,14 @@
 ﻿using Reactor.API.Configuration;
+using UnityEngine;
 
 namespace Reactor
 {
     internal static class Global
     {
+        internal static GameObject GameApiObject { get; set; }
         internal static Settings Settings { get; set; }
 
+        internal const string CentrifugeGameApiObjectName = "com.github.ciastex.CentrifugeGameAPI";
         internal const string InterceptUnityLogsSettingsKey = "InterceptUnityLogs";
     }
 }
