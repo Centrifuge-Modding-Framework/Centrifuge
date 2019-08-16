5332f36e8c5d937e5a27f383bc120ff347c3e6c9
vddCore <vddcore@outlook.com>
2019-08-16 12:58:33 +0200

    Changed a few things about the GameAPI... still not sure how to tackle this

---
+++ Reactor.API.GTTOD/Internal/Terminal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using GttodTerminal = CommandTerminal.Terminal;
+
+namespace Reactor.API.GTTOD.Internal
+{
+    internal class Terminal
+    {
+        private static GttodTerminal _reference;
+        public static GttodTerminal Reference
+        {
+            get
+            {
+                EnsureReferenceValid();
+                return _reference;
+            }
+        }
+        
+        private static void EnsureReferenceValid()
+        {
+            _reference = GameObject.FindObjectOfType<GttodTerminal>();
+
+            if (!_reference)
+            {
+                var termGameObject = new GameObject("com.github.ciastex/ReactorGameAPI.ReplacementGameTerminal");
+                _reference = termGameObject.AddComponent<GttodTerminal>();
+            }
+        }
+    }
+}
