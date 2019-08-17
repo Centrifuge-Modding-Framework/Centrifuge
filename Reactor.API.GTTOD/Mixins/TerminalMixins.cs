f6e6483fe3d5938c478e4d2c0bd65b9daecb2f9f
Ciastex <Ciastex@users.noreply.github.com>
2019-08-18 00:04:01 +0200

    Added some more terminal hooking functionality.

--- Reactor.API.GTTOD/Mixins/TerminalMixins.cs
+++ Reactor.API.GTTOD/Mixins/TerminalMixins.cs
@@ -29,5 +29,57 @@ namespace Reactor.API.GTTOD.Mixins
 
             return true;
         }
+
+        public static void Postfix()
+        {
+            Internal.Terminal.OnInitFinished();
+        }
+    }
+
+    [HarmonyPatch(typeof(Terminal), "SetupInput")]
+    internal class TerminalSetupInputMixin
+    {
+        public static void Postfix(Terminal __instance)
+        {
+            Internal.Terminal.OnInputStyleSet(__instance.input_style);
+        }
+    }
+
+    [HarmonyPatch(typeof(Terminal), "SetupWindow")]
+    internal class TerminalSetupWindowMixin
+    {
+        public static void Postfix(Terminal __instance)
+        {
+            Internal.Terminal.OnWindowStyleSet(__instance.window_style);
+        }
+    }
+
+    [HarmonyPatch(typeof(Terminal), "SetupLabels")]
+    internal class TerminalSetupLabelsMixin
+    {
+        public static void Postfix(Terminal __instance)
+        {
+            Internal.Terminal.OnLabelStyleSet(__instance.label_style);
+        }
+    }
+
+    [HarmonyPatch(typeof(Terminal), "ToggleState")]
+    internal class TerminalToggleStateMixin
+    {
+        public static void Postfix(Terminal __instance)
+        {
+            if (__instance.state == TerminalState.Close)
+            {
+                Internal.Terminal.OnClosed(__instance);
+            }
+            else
+            {
+                // FIXME: might trigger second time
+                //        if user expands from small 
+                //        size to large size
+
+                Internal.Terminal.OnOpened(__instance);
+            }
+        }
     }
 }
