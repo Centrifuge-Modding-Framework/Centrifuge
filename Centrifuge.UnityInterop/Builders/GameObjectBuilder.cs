1738194f5b2d30df146abd9322256433a2d16fce
vddCore <vddcore@outlook.com>
2019-10-17 07:49:28 +0200

    Guess we're doing it the hard way.

---
+++ Centrifuge.UnityInterop/Builders/GameObjectBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Centrifuge.UnityInterop.Builders
+{
+    public class GameObjectBuilder
+    {
+        private static Type _gameObjectType;
+
+        static GameObjectBuilder()
+        {
+        }
+
+        private static Type FindTypeByName(string name)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                                      .Where(asm => asm.GetName().FullName.StartsWith("UnityEngine"));
+
+
+        }
+    }
+}
