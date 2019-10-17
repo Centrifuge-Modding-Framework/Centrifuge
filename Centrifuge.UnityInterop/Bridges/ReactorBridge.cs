f6ff6f09ad945c2317bd3a81fe6adc7ee1bd4a61
Ciastex <mrciastexx@gmail.com>
2019-10-17 13:32:36 +0200

    Shit doesn't work, will fix later.

--- Centrifuge.UnityInterop/Bridges/ReactorBridge.cs
+++ Centrifuge.UnityInterop/Bridges/ReactorBridge.cs
@@ -5,5 +5,6 @@ namespace Centrifuge.UnityInterop.Bridges
     public static class ReactorBridge
     {
         public static Type ReactorManagerType = Kernel.FindTypeByFullName(Resources.ReactorManagerTypeName);
+        public static Type ReactorGlobalType = Kernel.FindTypeByFullName(Resources.ReactorGlobalTypeName);
     }
 }
--- Reactor/Global.cs
+++ Reactor/Global.cs
@@ -2,11 +2,14 @@
 
 namespace Reactor
 {
-    internal static class Global
+    public static class Global
     {
+        public static bool InterceptUnityLogs;
+
         internal const string InterceptUnityLogsSettingsKey = "InterceptUnityLogs";
 
         internal static object GameApiObject { get; set; }
         internal static Settings Settings { get; set; }
+
     }
 }
