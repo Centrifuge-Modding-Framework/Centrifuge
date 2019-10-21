480f8b92706c8acdc87f8fee8ed24ddf2d32ed3d
vddCore <vddcore@outlook.com>
2019-10-21 07:53:14 +0200

    Fix a bug... Who knew colors don't exist in the past :P

--- Centrifuge/EarlyLog.cs
+++ Centrifuge/EarlyLog.cs
@@ -2,32 +2,25 @@
 
 namespace Centrifuge
 {
-    internal static class EarlyLog
+    public static class EarlyLog
     {
         public static void Info(string message)
         {
-            Console.ForegroundColor = ConsoleColor.White;
             WriteMessage('i', message);
-            Console.ResetColor();
         }
 
         public static void Warning(string message)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
             WriteMessage('*', message);
-            Console.ResetColor();
         }
 
         public static void Error(string message)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
             WriteMessage('!', message);
-            Console.ResetColor();
         }
 
         public static void Exception(Exception e)
         {
-            Console.ForegroundColor = ConsoleColor.Magenta;
             WriteMessage('e', e.Message);
 
             if (!string.IsNullOrEmpty(e.StackTrace))
@@ -35,14 +28,9 @@ namespace Centrifuge
 
             if (e.InnerException != null)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("-------------- INNER EXCEPTION FOLLOWS --------------");
-                Console.ForegroundColor = ConsoleColor.Magenta;
-
                 Exception(e.InnerException);
             }
-
-            Console.ResetColor();
         }
 
         private static void WriteMessage(char symbol, string message)
