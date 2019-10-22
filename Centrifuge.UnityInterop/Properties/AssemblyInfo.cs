1ddda352676ed39ad56b19414caa0121e992d392
Ciastex <mrciastexx@gmail.com>
2019-10-22 06:02:03 +0200

    Update nuspec

--- Centrifuge.UnityInterop/Properties/AssemblyInfo.cs
+++ Centrifuge.UnityInterop/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Centrifuge.UnityInterop")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unity glue code for Centrifuge Mod Loader")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Centrifuge Project")]
 [assembly: AssemblyProduct("Centrifuge.UnityInterop")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
