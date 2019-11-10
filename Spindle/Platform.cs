8fa6bb1556aacc8d19a87c3fb0cf076bbd5fc7e5
vddCore <vddcore@outlook.com>
2019-11-11 00:07:46 +0100

    Fix for Mono on Linux.

---
+++ Spindle/Platform.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Spindle
+{
+    internal class Platform
+    {
+        internal static bool IsMono()
+        {
+            var platformID = (int)Environment.OSVersion.Platform;
+            return platformID == 4 || platformID == 6 || platformID == 128;
+        }
+
+        internal static bool IsUnix()
+        {
+            var platformID = Environment.OSVersion.Platform;
+            switch (platformID)
+            {
+                case PlatformID.MacOSX:
+                case PlatformID.Unix:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
