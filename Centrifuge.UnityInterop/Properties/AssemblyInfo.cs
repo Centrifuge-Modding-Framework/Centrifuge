08609b6074482e0ee9224635988d676f5df8bac4
Tomasz Cichoń <Ciastex@users.noreply.github.com>
2019-11-11 13:08:29 +0100

    Migrated all projects to the SDK project format.

--- Centrifuge.UnityInterop/Properties/AssemblyInfo.cs
+++ Centrifuge.UnityInterop/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Centrifuge.UnityInterop")]
-[assembly: AssemblyDescription("Unity glue code for Centrifuge Mod Loader")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Centrifuge Project")]
-[assembly: AssemblyProduct("Centrifuge.UnityInterop")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8dcc793a-bc56-4f07-a600-c0c109230c07")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
