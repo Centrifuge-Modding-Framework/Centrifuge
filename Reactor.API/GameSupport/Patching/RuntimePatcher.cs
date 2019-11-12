f0afcd9cee2e79b699afe877925986279850f132
vddCore <vddcore@outlook.com>
2019-11-12 15:26:57 +0100

    Add integrated runtime Harmony transpiler initialization.

---
+++ Reactor.API/GameSupport/Patching/RuntimePatcher.cs
@@ -0,0 +1,37 @@
+﻿using Harmony;
+using Reactor.API.Logging;
+using System;
+using System.Reflection;
+
+namespace Reactor.API.GameSupport.Patching
+{
+    public static class RuntimePatcher
+    {
+        public static HarmonyInstance HarmonyInstance { get; }
+        
+        private static Log Log { get; }
+
+        static RuntimePatcher()
+        {
+            HarmonyInstance = HarmonyInstance.Create(Defaults.ReactorModLoaderNamespace);
+            Log = new Log("RuntimePatcher");
+        }
+
+        public static void InitializeTranspilers()
+        {
+            var asm = Assembly.GetCallingAssembly();
+            var types = asm.GetTypes();
+
+            foreach (var type in types)
+            {
+                if (typeof(GameCodeTranspiler).IsAssignableFrom(type) && type != typeof(GameCodeTranspiler))
+                {
+                    var transpiler = Activator.CreateInstance(type) as GameCodeTranspiler;
+
+                    Log.Info($"Transpiler: {type.FullName}");
+                    transpiler.Apply(HarmonyInstance);
+                }
+            }
+        }
+    }
+}
