8b92fae9aae50ef9fae015a9d1fbd9cb808a5815
Tomasz Cichoń <Ciastex@users.noreply.github.com>
2019-11-12 15:26:57 +0100

    Add integrated runtime Harmony transpiler initialization.

---
+++ Reactor.API/GameSupport/Patching/GameCodeTranspiler.cs
@@ -0,0 +1,9 @@
+﻿using Harmony;
+
+namespace Reactor.API.GameSupport.Patching
+{
+    public abstract class GameCodeTranspiler
+    {
+        public abstract void Apply(HarmonyInstance harmonyInstance);
+    }
+}
