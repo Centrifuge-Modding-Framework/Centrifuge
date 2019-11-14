b6ef59e4641d3194a143ae3f305d53f3f843280f
vddCore <vddcore@outlook.com>
2019-11-14 16:45:32 +0100

    Move Unity log interception to a separate class.

--- Reactor/Global.cs
+++ Reactor/Global.cs
@@ -6,10 +6,8 @@ namespace Reactor
     internal class Global
     {
         internal const string InterceptUnityLogsSettingsKey = "InterceptUnityLogs";
-        internal const string UseConsolidatedLogSettingsKey = "UseConsolidatedLog";
 
         internal static bool InterceptUnityLogs { get; set; }
-        internal static bool UseConsolidatedLog { get; set; }
 
         internal static Dictionary<string, object> GameApiObjects { get; set; }
         internal static Settings Settings { get; set; }
