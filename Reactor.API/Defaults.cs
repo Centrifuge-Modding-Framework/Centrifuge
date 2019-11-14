29a01413961b222ac0743dcae303f799d1033baf
vddCore <vddcore@outlook.com>
2019-11-14 16:47:29 +0100

    Remove unneeded constants.

--- Reactor.API/Defaults.cs
+++ Reactor.API/Defaults.cs
@@ -11,27 +11,13 @@ namespace Reactor.API
         public static string PrivateSettingsDirectory => "Settings";
         public static string PrivateAssetsDirectory => "Assets";
         public static string PrivateDataDirectory => "Data";
-        public static string PrivateLogDirectory => "Logs";
 
         public static string ManagerSettingsDirectory => Path.Combine(CentrifugeRoot, "Settings");
         public static string ManagerGameSupportDirectory => Path.Combine(CentrifugeRoot, "GameSupport");
         public static string ManagerModDirectory => Path.Combine(CentrifugeRoot, "Mods");
         public static string ManagerLogDirectory => Path.Combine(CentrifugeRoot, "Logs");
 
-        public static string ConsolidatedLogFilePath => Path.Combine(ManagerLogDirectory, ConsolidatedLogFileName);
-
-        public const string DependencyResolverLogFileName = "DependencyResolver";
-        public const string RuntimeAssetLoaderLogFileName = "RuntimeAssetLoader";
-        public const string GameSupportInitializerLogFileName = "GameSupport";
-        public const string SettingsSystemLogFileName = "SettingsSystem";
-        public const string HotkeyManagerLogFileName = "HotkeyManager";
-        public const string FileSystemLogFileName = "FileSystem";
-        public const string MessengerLogFileName = "Messenger";
-        public const string ModLoaderLogFileName = "Loader";
-        public const string ManagerLogFileName = "Manager";
-
         public const string ManifestFileName = "mod.json";
-        public const string ConsolidatedLogFileName = "CommonLog.txt";
 
         public const string ReactorModLoaderNamespace = "com.github.ciastex.ReactorModLoader";
     }
