b8af272f5fdb526826954477505bac338cc36b21
Tomasz Cichoń <Ciastex@users.noreply.github.com>
2019-11-14 10:09:50 +0100

    Support selective coloring in decorators for console sinks.

--- Reactor.API/Logging/Decorators/DateTimeDecorator.cs
+++ Reactor.API/Logging/Decorators/DateTimeDecorator.cs
@@ -1,13 +1,20 @@
 ﻿using Reactor.API.Logging.Base;
+using Reactor.API.Extensions;
 using System;
+using Reactor.API.Logging.Sinks;
 
 namespace Reactor.API.Logging.Decorators
 {
     public class DateTimeDecorator : Decorator
     {
-        public override string Decorate(LogLevel logLevel, string input)
+        public override string Decorate(LogLevel logLevel, string input, Sink sink)
         {
-            return DateTime.Now.ToString();
+            var dateTimeString = DateTime.Now.ToString();
+
+            if (sink is ConsoleSink)
+                return dateTimeString.AnsiColorEncodeRGB(63, 63, 63);
+
+            return dateTimeString;
         }
     }
 }
--- Reactor.API/Logging/Decorators/LogLevelDecorator.cs
+++ Reactor.API/Logging/Decorators/LogLevelDecorator.cs
@@ -1,22 +1,32 @@
 ﻿using Reactor.API.Logging.Base;
+using Reactor.API.Extensions;
+using Reactor.API.Logging.Sinks;
 
 namespace Reactor.API.Logging.Decorators
 {
     public class LogLevelDecorator : Decorator
     {
-        public override string Decorate(LogLevel logLevel, string input)
+        public override string Decorate(LogLevel logLevel, string input, Sink sink)
         {
             return logLevel switch
             {
-                LogLevel.Info => "INF",
-                LogLevel.Warning => "WRN",
-                LogLevel.Error => "ERR",
-                LogLevel.Debug => "DBG",
-                LogLevel.Exception => "EXC",
-                LogLevel.ReflectionTypeLoadException => "RTL",
+                LogLevel.Info => EncodeAnsiIfConsole("INF", sink, 255, 255, 255),
+                LogLevel.Warning => EncodeAnsiIfConsole("WRN", sink, 255, 255, 0),
+                LogLevel.Error => EncodeAnsiIfConsole("ERR", sink, 255, 0, 0),
+                LogLevel.Debug => EncodeAnsiIfConsole("DBG", sink, 255, 0, 255),
+                LogLevel.Exception => EncodeAnsiIfConsole("EXC", sink, 0, 255, 255),
+                LogLevel.ReflectionTypeLoadException => EncodeAnsiIfConsole("RTL", sink, 0, 255, 255),
 
                 _ => "???"
             };
+        }
+
+        private string EncodeAnsiIfConsole(string s, Sink sink, byte r, byte g, byte b)
+        {
+            if (sink is ConsoleSink)
+                return s.AnsiColorEncodeRGB(r, g, b);
+
+            return s;
         }
     }
 }
