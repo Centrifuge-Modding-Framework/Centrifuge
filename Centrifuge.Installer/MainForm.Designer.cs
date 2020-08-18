87e95f0109a0fd503d1d9804b806ee9cb12ea4e4
vddCore <vddcore@outlook.com>
2020-08-18 23:11:36 +0200

    Add installer.

---
+++ Centrifuge.Installer/MainForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Centrifuge.Installer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this._pathTextBox = new System.Windows.Forms.TextBox();
+            this.browseGameButton = new System.Windows.Forms.Button();
+            this.dotnetStandardCheckBox = new System.Windows.Forms.CheckBox();
+            this._installButton = new System.Windows.Forms.Button();
+            this._folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this._mainPanel = new System.Windows.Forms.Panel();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this._toolStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this._toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.checkBoxToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this._mainPanel.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game location";
+            // 
+            // _pathTextBox
+            // 
+            this._pathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._pathTextBox.Location = new System.Drawing.Point(7, 24);
+            this._pathTextBox.Name = "_pathTextBox";
+            this._pathTextBox.ReadOnly = true;
+            this._pathTextBox.Size = new System.Drawing.Size(242, 20);
+            this._pathTextBox.TabIndex = 2;
+            this._pathTextBox.TextChanged += new System.EventHandler(this.PathTextBox_TextChanged);
+            // 
+            // browseGameButton
+            // 
+            this.browseGameButton.Location = new System.Drawing.Point(255, 24);
+            this.browseGameButton.Name = "browseGameButton";
+            this.browseGameButton.Size = new System.Drawing.Size(28, 20);
+            this.browseGameButton.TabIndex = 3;
+            this.browseGameButton.Text = "...";
+            this.browseGameButton.UseVisualStyleBackColor = true;
+            this.browseGameButton.Click += new System.EventHandler(this.BrowseGameButton_Click);
+            // 
+            // dotnetStandardCheckBox
+            // 
+            this.dotnetStandardCheckBox.AutoSize = true;
+            this.dotnetStandardCheckBox.Location = new System.Drawing.Point(7, 53);
+            this.dotnetStandardCheckBox.Name = "dotnetStandardCheckBox";
+            this.dotnetStandardCheckBox.Size = new System.Drawing.Size(152, 17);
+            this.dotnetStandardCheckBox.TabIndex = 4;
+            this.dotnetStandardCheckBox.Text = "Install .NET Standard build";
+            this.checkBoxToolTip.SetToolTip(this.dotnetStandardCheckBox, "Check only if your game uses Unity Engine 2017+");
+            this.dotnetStandardCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // _installButton
+            // 
+            this._installButton.Enabled = false;
+            this._installButton.Location = new System.Drawing.Point(208, 49);
+            this._installButton.Name = "_installButton";
+            this._installButton.Size = new System.Drawing.Size(75, 23);
+            this._installButton.TabIndex = 5;
+            this._installButton.Text = "Install";
+            this._installButton.UseVisualStyleBackColor = true;
+            this._installButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // _mainPanel
+            // 
+            this._mainPanel.Controls.Add(this.statusStrip);
+            this._mainPanel.Controls.Add(this._installButton);
+            this._mainPanel.Controls.Add(this.label1);
+            this._mainPanel.Controls.Add(this.dotnetStandardCheckBox);
+            this._mainPanel.Controls.Add(this._pathTextBox);
+            this._mainPanel.Controls.Add(this.browseGameButton);
+            this._mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._mainPanel.Location = new System.Drawing.Point(0, 0);
+            this._mainPanel.Name = "_mainPanel";
+            this._mainPanel.Size = new System.Drawing.Size(289, 98);
+            this._mainPanel.TabIndex = 6;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._toolStripLabel,
+            this._toolStripProgressBar});
+            this.statusStrip.Location = new System.Drawing.Point(0, 76);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(289, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 6;
+            // 
+            // _toolStripLabel
+            // 
+            this._toolStripLabel.Name = "_toolStripLabel";
+            this._toolStripLabel.Size = new System.Drawing.Size(133, 17);
+            this._toolStripLabel.Text = "Latest release: unknown";
+            // 
+            // _toolStripProgressBar
+            // 
+            this._toolStripProgressBar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this._toolStripProgressBar.AutoSize = false;
+            this._toolStripProgressBar.Margin = new System.Windows.Forms.Padding(4, 4, 1, 3);
+            this._toolStripProgressBar.MarqueeAnimationSpeed = 50;
+            this._toolStripProgressBar.Name = "_toolStripProgressBar";
+            this._toolStripProgressBar.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this._toolStripProgressBar.Size = new System.Drawing.Size(100, 15);
+            this._toolStripProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this._toolStripProgressBar.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 98);
+            this.Controls.Add(this._mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Centrifuge Installer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this._mainPanel.ResumeLayout(false);
+            this._mainPanel.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox _pathTextBox;
+        private System.Windows.Forms.Button browseGameButton;
+        private System.Windows.Forms.CheckBox dotnetStandardCheckBox;
+        private System.Windows.Forms.Button _installButton;
+        private System.Windows.Forms.FolderBrowserDialog _folderBrowserDialog;
+        private System.Windows.Forms.Panel _mainPanel;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel _toolStripLabel;
+        private System.Windows.Forms.ToolStripProgressBar _toolStripProgressBar;
+        private System.Windows.Forms.ToolTip checkBoxToolTip;
+    }
+}
---
+++ Centrifuge.Installer/MainForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Octokit;
+
+namespace Centrifuge.Installer
+{
+    public partial class MainForm : Form
+    {
+        private readonly GitHubClient _githubClient;
+        private Release _latestRelease;
+        private string _gameDataDir;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _githubClient = new GitHubClient(new ProductHeaderValue("Centrifuge.Installer", "1.0"));
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            _toolStripLabel.Text = "Latest release: retrieving...";
+
+            SetBusyState(true);
+            _latestRelease = await _githubClient.Repository.Release.GetLatest("Ciastex", "Centrifuge");
+            SetBusyState(false);
+
+            _toolStripLabel.Text = $"Latest release: {_latestRelease.Name}";
+        }
+
+        private void BrowseGameButton_Click(object sender, EventArgs e)
+        {
+            _folderBrowserDialog.ShowNewFolderButton = false;
+            _folderBrowserDialog.Description = "Browse for the game directory (the one containing _Data directory)...";
+
+            var result = _folderBrowserDialog.ShowDialog(this);
+
+            if (result == DialogResult.OK)
+            {
+                _pathTextBox.Text = _folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private async void InstallButton_Click(object sender, EventArgs e)
+        {
+            SetBusyState(true);
+
+            try
+            {
+                var targetPath = Path.Combine(Path.GetTempPath(), "centrifuge.zip");
+
+                ReleaseAsset asset;
+                if (dotnetStandardCheckBox.Checked)
+                {
+                    asset = _latestRelease.Assets.First(x => x.Name.Contains("netstandard"));
+                }
+                else
+                {
+                    asset = _latestRelease.Assets.First(x => x.Name.Contains("net35"));
+                }
+
+                using (var webClient = new WebClient())
+                {
+                    await webClient.DownloadFileTaskAsync(asset.BrowserDownloadUrl, targetPath);
+                }
+
+                using (var zipArchive = ZipFile.OpenRead(targetPath))
+                {
+                    zipArchive.ExtractToDirectory(Path.Combine(_pathTextBox.Text, _gameDataDir));
+                }
+
+                var spindleProcess = new Process()
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        Arguments = "-t Assembly-CSharp.dll -s Centrifuge.dll",
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                        WorkingDirectory = Path.Combine(_gameDataDir, "Managed"),
+                        FileName = Path.Combine(_gameDataDir, "Managed", "Spindle.exe")
+                    }
+                };
+
+                await Task.Run(() =>
+                {
+                    spindleProcess.Start();
+                    spindleProcess.WaitForExit();
+                });
+
+                if (spindleProcess.ExitCode != 0)
+                {
+                    MessageBox.Show(this, $"Spindle terminated with exit code {spindleProcess.ExitCode}: ({(TerminationReason)spindleProcess.ExitCode})", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            SetBusyState(false);
+        }
+
+        private void SetBusyState(bool busy)
+        {
+            if (busy)
+            {
+                _mainPanel.Enabled = false;
+                _toolStripProgressBar.Visible = true;
+            }
+            else
+            {
+                _mainPanel.Enabled = true;
+                _toolStripProgressBar.Visible = false;
+            }
+        }
+
+        private void PathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            _installButton.Enabled = ValidateGameDirectory(_pathTextBox.Text, out _gameDataDir);
+        }
+        
+        private bool ValidateGameDirectory(string path, out string dirOfInterest)
+        {
+            var dirs = Directory.GetDirectories(path);
+            dirOfInterest = dirs.FirstOrDefault(x => x.EndsWith("_Data"));
+
+            return !string.IsNullOrWhiteSpace(path) && dirOfInterest != null;
+        }
+    }
+}
