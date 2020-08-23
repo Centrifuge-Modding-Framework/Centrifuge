580177ec5f99c1313cac769b3fecb97b9c19c353
vddCore <vddcore@outlook.com>
2020-08-23 13:31:32 +0200

    Add missing .NET 3.5 binary and buff release script a bit...

--- pack_release.sh
+++ pack_release.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+
+art="./__BUILD/artifact"
+shared="./Centrifuge.Shared/Binaries"
 net35_bin="./__BUILD/Release/net35"
-net35_art="./__BUILD/artifact/net35"
+net35_art="$art/net35"
 
 std20_bin="./__BUILD/Release/netstandard20"
-std20_art="./__BUILD/artifact/netstandard20"
+std20_art="$art/netstandard20"
 
-rm -rf $net35_art 2> /dev/null
-rm -rf $std20_art 2> /dev/null
+installer_bin="./__BUILD/installer/Release/net461"
+
+api_ver=$(sed -n 's/<Version>\(.*\)<\/Version>/\1/p' Reactor.API/Reactor.API.csproj | awk '{$1=$1};1')
+
+platform="$(uname -s)"
+case "${platform}" in
+    Linux*)     machine="Linux";;
+    Darwin*)    machine="Mac";;
+    CYGWIN*)    machine="Cygwin";;
+    MINGW*)     machine="MinGw";;
+    *)          machine="Unsupported"
+esac
+
+rm -rf $art 2> /dev/null
 
 mkdir -p $net35_art/Centrifuge
 mkdir -p $net35_art/Managed
@@ -25,6 +40,7 @@ cp $net35_bin/MonoMod.Utils.dll $net35_art/Managed/
 cp $net35_bin/Spindle.exe $net35_art/Managed/
 cp $net35_bin/install_windows.bat $net35_art/Managed/
 cp $net35_bin/install_linux.sh $net35_art/Managed/
+cp $shared/System.Runtime.Serialization.dll $net35_art/Managed/
 
 cp $std20_bin/Reactor.dll $std20_art/Centrifuge/
 cp $std20_bin/Reactor.API.dll $std20_art/Centrifuge/
@@ -39,3 +55,30 @@ cp $std20_bin/Spindle.dll $std20_art/Managed/Spindle.exe
 cp $std20_bin/install_windows.bat $std20_art/Managed/
 cp $std20_bin/install_linux.sh $std20_art/Managed/
 cp $std20_bin/System.* $std20_art/Managed/
+
+cd $net35_art
+echo "Packaging .NET 3.5 build..."
+zip -rj9 ../Centrifuge.$api_ver.net35.zip Centrifuge Managed
+cd ../../../
+echo
+
+cd $std20_art
+echo "Packaging .NET Standard 2.0 build..."
+zip -rj9 ../Centrifuge.$api_ver.netstandard.zip Centrifuge Managed
+cd ../../../
+echo
+
+if [ "$platform" == "Linux" ] || [ "$platform" == "Mac" ] || [ "$platform" == "Unsupported" ]; then
+    echo "Skipping installer packaging as we're not running on Windows..."
+else
+    echo "Packaging installer..."
+
+    cd $installer_bin
+    zip -rj9 ../../../../$art/Centrifuge.Installer.zip .
+    cd ../../../../
+    echo
+fi
+
+stat -c "[%s bytes] %n" $art/Centrifuge.$api_ver.net35.zip
+stat -c "[%s bytes] %n" $art/Centrifuge.$api_ver.netstandard.zip
+stat -c "[%s bytes] %n" $art/Centrifuge.Installer.zip
