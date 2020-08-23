0713423c24e9368952a580964ab0d5076f4c8ac1
vddCore <vddcore@outlook.com>
2020-08-23 10:40:16 +0200

    Obsolete MessageHandlerAttribute as well

--- Reactor.API/Attributes/MessageHandlerAttribute.cs
+++ Reactor.API/Attributes/MessageHandlerAttribute.cs
@@ -2,6 +2,8 @@
 
 namespace Reactor.API.Attributes
 {
+    [Obsolete("Use IManager.GetMod(string modId).Instance to communicate between the mods instead." +
+              "\nThis will be removed in Centrifuge 4.0.")]
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class MessageHandlerAttribute : Attribute
     {
