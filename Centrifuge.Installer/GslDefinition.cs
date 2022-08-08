fafb89c1da30d8654e453771ac355dbdda25afa2
vddCore <t.cichon.64@outlook.com>
2022-08-08 18:35:44 +0200

    Add support for automated GSL installation.

---
+++ Centrifuge.Installer/GslDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Centrifuge.Installer
+{
+    public class GslDefinition
+    {
+        [JsonPropertyName("brief")]
+        public string Brief { get; set; }
+        
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("data")]
+        public Dictionary<string, string> Data { get; set; }
+    }
+}
